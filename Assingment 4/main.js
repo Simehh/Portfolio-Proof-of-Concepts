8ed10ee7cf942139c05f861e28243dc471a0d129
Simehh <simeon.samuels@dcmail.ca>
2024-04-09 11:27:53 -0400

    adding the items/variables for the randomizer

--- Assingment 4/main.js
+++ Assingment 4/main.js
@@ -11,5 +11,8 @@ randomize.addEventListener('click', result);
 
 function result() {
     let newStory = storyText;
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
 
 }
